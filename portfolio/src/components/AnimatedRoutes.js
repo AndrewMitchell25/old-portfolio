ff4de208c14bc57b3841582be4cb0742d31ad1c2
AndrewMitchell25 <andrew.j.mitchell.247@gmail.com>
2022-05-11 02:29:02 -0400

    work on design

--- portfolio/src/components/AnimatedRoutes.js
+++ portfolio/src/components/AnimatedRoutes.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {Route, Routes, useLocation} from "react-router-dom";
 import Home from '../pages/Home';
-import ContactMe from '../pages/ContactMe';
 import Resume from '../pages/Resume';
 import {AnimatePresence} from "framer-motion";
 
@@ -13,7 +12,6 @@ export default function AnimatedRoutes(){
         <AnimatePresence exitBeforeEnter={true} initial={false}>
             <Routes location={location} key={location.pathname}>
                 <Route path="/" element={<Home />} />
-                <Route path="/contact" element={<ContactMe />} />
                 <Route path="/resume" element={<Resume />} />
             </Routes>
         </AnimatePresence>
